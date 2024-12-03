686c2d738193f47f70e4adea42bcb4996883e2d3
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-12-03 20:25:19 +0000

    sass added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import "./App.css";
+// import "./App.css";
+// import "./scss/main.css";
+// import "./scss/main.scss";
+// import "./scss/components/_App.scss";
+
+// function App() {
+//   return (
+//     <>
+//       <p className="saasVariable">Hello world</p>
+//       <p className="variableFavoriteColor">My favorite color is lightgreen</p>
+//       <span className="circle">Mixin</span>
+//       <span className="circle">Mixin</span>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import "./styles/main.scss";
+import Buttons from "./components/Buttons/Buttons.jsx";
 
 function App() {
   return (
-    <>
-      <p className="saasVariable">Hello world</p>
-      <p className="variableFavoriteColor">My favorite color is lightgreen</p>
-    </>
+    <div className="App">
+      <Buttons>Click me</Buttons>
+    </div>
   );
 }
 
