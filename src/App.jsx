9926d80aba51fb33f42d087df3c701110f29beab
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-12-03 15:08:09 +0000

    new scss rules added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="title">Hello world</h1>
+      <p className="saasVariable">Hello world</p>
+      <p className="variableFavoriteColor">My favorite color is lightgreen</p>
     </>
   );
 }
